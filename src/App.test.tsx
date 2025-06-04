b6b4ebb4c295857f88a93f236d81822fc92d59c8
RubenCastaneda <therubencastaneda@gmail.com>
2025-06-04 11:30:13 -0500

    test: assert header renders

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders KK Beauty header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText('KK Beauty');
+  expect(headerElement).toBeInTheDocument();
 });
