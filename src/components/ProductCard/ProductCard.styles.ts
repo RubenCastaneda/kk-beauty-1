eb22a84ecf083bc8acaa3e2ddb1cf024026cff1f
RubenCastaneda <therubencastaneda@gmail.com>
2025-08-13 15:31:37 -0500

    style: improve featured products layout

--- src/components/ProductCard/ProductCard.styles.ts
+++ src/components/ProductCard/ProductCard.styles.ts
@@ -17,6 +17,7 @@ export const Card = styled.div`
   position: relative;
   overflow: hidden;
   cursor: pointer;
+  width: 100%;
   border-radius: 0; /* sharp corners */
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
   transition:
